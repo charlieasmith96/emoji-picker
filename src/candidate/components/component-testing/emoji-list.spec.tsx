ef676c77bb2783c2787baf47b1c214b7811b897a
Charlie Smith <charliesmith@Charlies-MacBook-Pro.local>
2023-06-04 16:45:00 +0200

    style: final prettier pass

--- src/candidate/components/component-testing/emoji-list.spec.tsx
+++ src/candidate/components/component-testing/emoji-list.spec.tsx
@@ -2,20 +2,30 @@
  * @jest-environment jsdom
  */
 
-import { render, screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react";
 
-import { EmojiList } from "../EmojiList"
-import { emojiTestFixture } from "./test-fixture"
+import { EmojiList } from "../EmojiList";
+import { emojiTestFixture } from "./test-fixture";
 
-test('loads and displays all options', async () => {
-    render(<EmojiList emojis={emojiTestFixture} onItemSelect={() => console.log('test')}/>)
-  
-  
-    const firstOption = await screen.queryByRole('option', { name: /grinning face/i });
-    const secondOption = await screen.queryByRole('option', { name: /confused face/i });
-    const thirdOption = await screen.queryByRole('option', { name: /dizzy face/i });
-  
-    expect(firstOption).toBeTruthy();
-    expect(secondOption).toBeTruthy();
-    expect(thirdOption).toBeTruthy();
-  })
+test("loads and displays all options", async () => {
+  render(
+    <EmojiList
+      emojis={emojiTestFixture}
+      onItemSelect={() => console.log("test")}
+    />
+  );
+
+  const firstOption = await screen.queryByRole("option", {
+    name: /grinning face/i,
+  });
+  const secondOption = await screen.queryByRole("option", {
+    name: /confused face/i,
+  });
+  const thirdOption = await screen.queryByRole("option", {
+    name: /dizzy face/i,
+  });
+
+  expect(firstOption).toBeTruthy();
+  expect(secondOption).toBeTruthy();
+  expect(thirdOption).toBeTruthy();
+});
--- src/candidate/components/component-testing/fancy-input.spec.tsx
+++ src/candidate/components/component-testing/fancy-input.spec.tsx
@@ -2,68 +2,73 @@
  * @jest-environment jsdom
  */
 
-import { render, screen } from "@testing-library/react"
-import userEvent from '@testing-library/user-event'
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import { FancyInput } from "../FancyInput"
+import { FancyInput } from "../FancyInput";
 
-test('search, selects and inserts smiling face, whilst hiding popover', async () => {
-    render(<FancyInput placeholder="test-placeholder" />)
-  
-    const input = await screen.queryByRole('textbox') as HTMLInputElement;
+test("search, selects and inserts smiling face, whilst hiding popover", async () => {
+  render(<FancyInput placeholder="test-placeholder" />);
 
-    await userEvent.type(input, 'dog :smile')
+  const input = (await screen.queryByRole("textbox")) as HTMLInputElement;
 
-    const firstOption = await screen.queryByRole('option', { name: /grinning face with smiling eyes/i });
+  await userEvent.type(input, "dog :smile");
 
-    await userEvent.click(firstOption as HTMLElement);
+  const firstOption = await screen.queryByRole("option", {
+    name: /grinning face with smiling eyes/i,
+  });
 
-    expect(firstOption).not.toBeInTheDocument();
+  await userEvent.click(firstOption as HTMLElement);
 
-    expect(input).toHaveDisplayValue('dog 😄');
-  })
+  expect(firstOption).not.toBeInTheDocument();
 
-  test('should hide popover on left arrow press', async () => {
-    render(<FancyInput placeholder="test-placeholder" />)
-  
-    const input = await screen.queryByRole('textbox') as HTMLInputElement;
+  expect(input).toHaveDisplayValue("dog 😄");
+});
 
-    await userEvent.type(input, 'dog :smile')
+test("should hide popover on left arrow press", async () => {
+  render(<FancyInput placeholder="test-placeholder" />);
 
-    const firstOption = await screen.queryByRole('option', { name: /grinning face with smiling eyes/i });
+  const input = (await screen.queryByRole("textbox")) as HTMLInputElement;
 
-    await userEvent.type(input, '{arrowleft}')
+  await userEvent.type(input, "dog :smile");
 
-    expect(firstOption).not.toBeInTheDocument();
-  })
+  const firstOption = await screen.queryByRole("option", {
+    name: /grinning face with smiling eyes/i,
+  });
 
-  test('should hide popover on right arrow press', async () => {
-    render(<FancyInput placeholder="test-placeholder" />)
-  
-    const input = await screen.queryByRole('textbox') as HTMLInputElement;
+  await userEvent.type(input, "{arrowleft}");
 
-    await userEvent.type(input, 'dog :smile')
+  expect(firstOption).not.toBeInTheDocument();
+});
 
-    const firstOption = await screen.queryByRole('option', { name: /grinning face with smiling eyes/i });
+test("should hide popover on right arrow press", async () => {
+  render(<FancyInput placeholder="test-placeholder" />);
 
-    await userEvent.type(input, '{arrowright}')
+  const input = (await screen.queryByRole("textbox")) as HTMLInputElement;
 
-    expect(firstOption).not.toBeInTheDocument();
-  })
+  await userEvent.type(input, "dog :smile");
 
+  const firstOption = await screen.queryByRole("option", {
+    name: /grinning face with smiling eyes/i,
+  });
 
-  test('should hide popover on escape key press', async () => {
-    render(<FancyInput placeholder="test-placeholder" />)
-  
-    const input = await screen.queryByRole('textbox') as HTMLInputElement;
+  await userEvent.type(input, "{arrowright}");
 
-    await userEvent.type(input, 'dog :smile')
+  expect(firstOption).not.toBeInTheDocument();
+});
 
-    const firstOption = await screen.queryByRole('option', { name: /grinning face with smiling eyes/i });
+test("should hide popover on escape key press", async () => {
+  render(<FancyInput placeholder="test-placeholder" />);
 
-    await userEvent.type(input, '{escape}')
+  const input = (await screen.queryByRole("textbox")) as HTMLInputElement;
 
-    expect(firstOption).not.toBeInTheDocument();
-  })
+  await userEvent.type(input, "dog :smile");
 
+  const firstOption = await screen.queryByRole("option", {
+    name: /grinning face with smiling eyes/i,
+  });
 
+  await userEvent.type(input, "{escape}");
+
+  expect(firstOption).not.toBeInTheDocument();
+});
